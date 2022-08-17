297d15a6d13ef9c7a9b399e24d9c621cabb4ae8f
Sushrut Kuchik <62986960+KSushrut@users.noreply.github.com>
2022-08-17 11:11:50 +0530

    Add files via upload

--- ddos/collect.sh
+++ ddos/collect.sh
@@ -3,9 +3,9 @@ n=6    # number of switches
 h=9
 port=1
 threshold=80
-python create.py "${h}"	#create.py is used to create counter.txt which keeps count of the no of times an attack has occured and which is the attacker
-i=1 #iterating variable
-l=`python read.py`	#read from counte.txt
+python create.py "${h}"		#create.py is used to create counter.txt to keep count of no of times attack has occured and which host is the attacker
+i=1	#iterating variable
+l=`python read.py`		
 
 scores=$(echo $l | tr ";" "\n")
 IFS=','
@@ -24,6 +24,8 @@ score_dict[0a:0a:00:00:00:07]=${arr[6]}
 score_dict[0a:0a:00:00:00:08]=${arr[7]}
 score_dict[0a:0a:00:00:00:09]=${arr[8]}
 
+echo "Penalty given: $((score_dict[0a:0a:00:00:00:03]))"
+
 #dictionary to map mac address to the host
 declare -A mac_dict
 mac_dict[0a:0a:00:00:00:01]=h1
@@ -38,9 +40,12 @@ mac_dict[0a:0a:00:00:00:09]=h9
 
 for i in {1..2000}
 do
+	 t1=$(date +%s)
+	 echo "Time 1: $t1"
     for ((j = 1; j <= n; j++))
     do
-        echo "Inspection at s$j"			#echo "Inspection no. $i at s$j"
+
+        echo "Inspection no. $i at s$j"
         # extract essential data from raw data
         sudo ovs-ofctl dump-flows s$j > data/raw
         grep "nw_src" data/raw > data/flowentries.csv
@@ -52,7 +57,6 @@ do
         macdst=$(awk -F "," '{out=""; for(k=2;k<=NF;k++){out=out" "$k}; print out}' data/flowentries.csv | awk -F " " '{split($10,d,"="); print d[2]}')
         
         # check if there are no traffics in the network at the moment.
-        
         if test -z "$packets" || test -z "$bytes" || test -z "$ipsrc" || test -z "$ipdst" 
         then
             state=0
@@ -64,64 +68,72 @@ do
 		        printf "$macsrc" > data/macsrc.txt
 		        python3 computeTuples.py
 		        python3 inspector.py
-		        state=$(awk '{print $0;}' .result)				#state = 0(no attack) or 1(attack)
+		        state=$(awk '{print $0;}' .result)
 		        
-		        #the next condition checks if any of the host score is less than 100, if yes then increase using score_increase.py file.
-		        
+		        #the next condition checks if any of the score is less than 100 and is genuine traffic
+				
 				if [[ ${score_dict[0a:0a:00:00:00:01]} != "100" || ${score_dict[0a:0a:00:00:00:02]} != "100" || ${score_dict[0a:0a:00:00:00:03]} != "100" || ${score_dict[0a:0a:00:00:00:04]} != "100" || ${score_dict[0a:0a:00:00:00:05]} != "100" || ${score_dict[0a:0a:00:00:00:06]} != "100" || ${score_dict[0a:0a:00:00:00:07]} != "100" || ${score_dict[0a:0a:00:00:00:08]} != "100" || ${score_dict[0a:0a:00:00:00:09]} != "100" ]]
         		then		    		
-					python score_increase.py			
+					python score_increase.py		#proceed to increase the score
 		    	fi       
         fi
 
         if [ $state -eq 1 ];			#if attack is detected
         then
-            file='data/macsrc.txt'
-            echo "------------------------------------------"
+            file='data/macsrc.txt'		#mac address of attacker is found from the macsrc.txt file
             echo "Network is under attack occuring at s$j"            
             while read line; do   
 				attacker=$line
 				i=$((i+1))  
 				done < $file
 				att=${mac_dict[$attacker]}
+				echo ""
 	    		echo "Attacker is ${mac_dict[$attacker]}"
 				if [ ${score_dict[$attacker]} -gt $threshold ]		## if less than 80, suspend
 				then
 				    
-				    	############################# LOGIC FOR THRESHOLD #####################
+				    	# LOGIC FOR THRESHOLD #
 				    	
-				    	score_dict[$attacker]=$((score_dict[$attacker]-9))			#reduce score by 9
-				    	echo "Current score of the attacker is: $((score_dict[$attacker]))"			#print score
-				    	python score.py "${score_dict[$attacker]}" "$att"			#pass score and attacker host to score.py file to update in file
-				    	sudo ovs-ofctl mod-port s$j $port no-receive				#suspend the port
-				    	sleep 5														#wait for 5 seconds
-				    	echo "Port suspended for 5 sec"	      						
-    					echo "------------------------------------------"  	
-				    	sudo ovs-ofctl mod-port s$j $port receive					#enable the port again
-				else
-					    python logic.py "${att}"									#logic.py keeps a count of attacks
-				    	echo -e "Score has fallen below threshold! Intervention of admin required\n"
-				    	sudo ovs-ofctl mod-port s$j $port down						#down the port
-				    	sudo ovs-ofctl dump-flows s$j > data/rawblock				
-				    	echo "Admin, enter your decision, 0 to block, 1 to allow"
-				    	read choice													#0(keep it blocked), 1(allow to run)
+				    	score_dict[$attacker]=$((score_dict[$attacker]-9))		#attacker score reduced
+				    	echo "Score of attacker: $((score_dict[$attacker]))"		#print the score to terminal
+				    	python score.py "${score_dict[$attacker]}" "$att"		#pass the score of attacker and mac to the score.py file to update the score in the file
+				    	sudo ovs-ofctl mod-port s$j $port no-receive
+				    	sleep 5
+				    	echo "Port suspended for 5 sec"	        	
+				    	sudo ovs-ofctl mod-port s$j $port receive
+				    else
+					    python logic.py "${att}"
+				    	echo "Blocked"
+				    	t2=$(date +%s)
+				    	echo "Time 2: $t2"
+						answer=$(( t2 - t1 ))
+						echo "Time taken for inspection: $answer"
+				    	sudo ovs-ofctl mod-port s$j $port down
+				    	sudo ovs-ofctl dump-flows s$j > data/rawblock
+				    	echo "Enter 1 to reset"
+				    	read choice
 				    	if [ $choice -eq 1 ]
 				    	then
-				    		sudo ovs-ofctl mod-port s$j $port up					
+				    		sudo ovs-ofctl mod-port s$j $port up
 				    		score_dict[$attacker]=100
 				    		python score.py "${score_dict[$attacker]}" "$att"
 				    	else
-				    		echo "BLOCKED!!!!"
+				    		echo "Reset!"
+				    		    
 				    		sudo ovs-ofctl dump-flows s$j > data/rawblock2
-				    	fi															#end of if(choice by admin)
-            	fi																	#end of if(threshold)
+				    	fi
+            	fi
                                                        
             default_flow=$(sudo ovs-ofctl dump-flows s$j | tail -n 1)    # Get flow ] "action:CONTROLLER:<port_num>" sending unknown packet to the controller
             sudo ovs-ofctl del-flows s$j
             sudo ovs-ofctl add-flow s$j "$default_flow"
-        fi																			#end of if(attack is detected)
+        fi
     done
     sleep 3
+    t2=$(date +%s)
+    echo "Time 2: $t2"
+    answer=$(echo $(( t2 - t1 )))
+	echo "$answer"
 done
 
 
